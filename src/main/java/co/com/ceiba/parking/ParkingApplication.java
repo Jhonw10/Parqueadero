04c94141f9e94972559da72071fbc46c56a83777
SURAMERICANA\jhonolve <jhon.olarte@ceiba.com.co>
2018-06-13 09:54:50 -0500

    Ajuste deploy tomcat.

--- src/main/java/co/com/ceiba/parking/ParkingApplication.java
+++ src/main/java/co/com/ceiba/parking/ParkingApplication.java
@@ -1,12 +1,25 @@
 package co.com.ceiba.parking;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 
 @SpringBootApplication
-public class ParkingApplication{
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class ParkingApplication extends SpringBootServletInitializer{
 
+	@Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(ParkingApplication.class);
+	  }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ParkingApplication.class, args);
 	}
